98fe2a9220c50c9cc7384b05b9b76eaedcca600a
Chris <chris.eddy.dev@gmail.com>
2019-12-01 15:53:38 -0800

    Scene struct definition

---
+++ rust-wasm-exokit-avatars/src/three/scenes/Scene.rs
@@ -0,0 +1,28 @@
+pub struct Scene {
+    r#type: String,
+    background: String, // todo, is null in JS
+    fog: String, // todo, is null in JS
+    overrideMaterial: String, //todo is null in JS
+    autoUpdate: bool
+}
+
+impl Scene {
+
+    // TODO, I dont know the types for these params
+
+    // pub fn copy(&self, source, recursive) {
+    //     return
+    // }
+
+    // pub fn toJSON(&self, meta) {
+    //     return
+    // }
+
+    pub fn dispose(&self) {
+        return
+    }
+}
+
+pub fn main() {
+    return
+}
