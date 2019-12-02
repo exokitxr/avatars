58364d8ed23a294f8c0357a4da9a6896194b594a
Chris <chris.eddy.dev@gmail.com>
2019-12-01 17:15:13 -0800

    layout undecapitate()

--- rust-wasm-exokit-avatars/src/lib.rs
+++ rust-wasm-exokit-avatars/src/lib.rs
@@ -5,18 +5,28 @@ use wasm_bindgen::prelude::*;
 pub struct Avatar {
     decapitated: bool,
     springBoneManager: bool, //todo, not a bool
-    modelBones: bool // todo, not a bool
+    modelBones: bool, // todo, not a bool
+    debugMeshes: bool, // todo, not a bool
 }
 
 impl Avatar {
 
-    pub fn undecapitate(&self) {
-        if self.decapitated {
-            
-        }
+    pub fn undecapitate(&mut self) {
+    //     if self.decapitated {
+    //         self.modelBones.head.traverse( Fn(String) {
+    //             o.position.copy(o.savedPosition);
+    //             o.matrixWorld.copy(o.savedMatrixWorld);
+    //         });
+    //         if self.debugMeshes {
+    //             // need to add traits to debugMeshes
+    //             self.debugMeshes.eyes.mesh.visible = true;
+    //             self.debugMeshes.head.visible = true;
+    //         }
+    //         self.decapitated = false
+    //     }
     }
 
-    pub fn update(&self) {
+    pub fn update(&mut self) {
         let wasDecapitated: bool = self.decapitated;
         if self.springBoneManager && wasDecapitated {
             self.undecapitate();
