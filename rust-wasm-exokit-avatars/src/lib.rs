65b1d7703fb5849be0ae63c3777a02cbd11a9f8c
Chris <chris.eddy.dev@gmail.com>
2019-12-01 18:48:55 -0800

    more update() rewriting

--- rust-wasm-exokit-avatars/src/lib.rs
+++ rust-wasm-exokit-avatars/src/lib.rs
@@ -1,5 +1,8 @@
 extern crate wasm_bindgen;
+
 use wasm_bindgen::prelude::*;
+use std::time::{SystemTime};
+use std::cmp;
 
 #[wasm_bindgen]
 pub struct Avatar {
@@ -7,6 +10,7 @@ pub struct Avatar {
     springBoneManager: bool, //todo, not a bool
     modelBones: bool, // todo, not a bool
     debugMeshes: bool, // todo, not a bool
+    inputs: bool, // todo, not a bool
 }
 
 impl Avatar {
@@ -30,6 +34,61 @@ impl Avatar {
         let wasDecapitated: bool = self.decapitated;
         if self.springBoneManager && wasDecapitated {
             self.undecapitate();
+        }
+
+        // todo, not a bool
+        let modelScaleFactor: bool = self.inputs.hmd.scaleFactor;
+        if modelScaleFactor != self.lastModelScaleFactor {
+            self.model.scale.set(modelScaleFactor, modelScaleFactor, modelScaleFactor);
+            self.lastModelScaleFactor = modelScaleFactor;
+
+            for springBoneGroup in 0..self.springBoneManager.springBoneList.len() {
+                for springBone in 0..springBoneGroup.len() {
+                    springBone._worldBoneLength = springBone.bone
+                    // not sure if chaining is allowed in rust
+                    .localToWorld(localVector.copy(springBone._initialLocalChildPosition))
+                    .sub(springBone._worldPosition)
+                    .length();
+                }
+            }
+        }
+
+        self.shoulderTransforms.Update();
+        self.legsManager.Update();
+
+        for k in 0..self.modelBones.len() {
+            // todo assign types
+            let modelBone = self.modelBones[k];
+            let modelBoneOutput = self.modelBoneOutputs[k];
+
+            if k == "Hips" {
+                modelBone.position.copy(modelBoneOutput.position);
+            }
+
+            modelBone.quaternion.multiplyQuaternions(modelBoneOutput.quaternion, modelBone.initialQuaternion);
+
+            if k == "Left_ankle" || k == "Right_ankle" {
+                modelBone.quaternion.multiply(upRotation);
+            }
+            else if k == "Left_wrist" {
+                modelBone.quaternion.multiply(leftRotation); // center
+            }
+            else if k == "Right_wrist" {
+                modelBone.quaternion.multiply(rightRotation); // center
+            }
+            modelBone.updateMatrixWorld();
+
+            let now = SystemTime::now();
+            let timeDiff = cmp::min(now - self.lastTimeStamp, 1000)
+            self.lastTimeStamp = now;
+
+            if self.options.fingers {
+                pub fn _processFingerBones(left) {
+
+                }
+
+            }
+
         }
     }
 
